062dba8fe5712f00156b5e178af47d5b64a5ebd3
John <john@johngm.com>
2013-11-18 05:57:01 -0800

    Removed debug code

--- src/SandToy.cpp
+++ src/SandToy.cpp
@@ -42,24 +42,6 @@ SandToy::SandToy()
   gridView = new GridView(pixelDisplay);
   gridModel->addListener(gridView);
 
-  /*pixelDisplay->setPixel(glm::ivec2(5,5), glm::vec3(0.0,1.0,0.0));
-
-  std::array<std::array<double, 3>, 3> neighborNewMass = GridModel::massRedistribute(glm::dvec2(0.0,-0.5));
-  glm::ivec2 neighborIterator(0, 0);
-  for(neighborIterator.x = 0; neighborIterator.x < 3; neighborIterator.x++)
-  {
-    for(neighborIterator.y = 0; neighborIterator.y < 3; neighborIterator.y++)
-    {
-      double thisMass = neighborNewMass[neighborIterator.x][neighborIterator.y];
-      glm::vec3 color(glm::vec3(1.0,1.0,1.0) * float(thisMass));
-      //color = glm::vec3(float(neighborIterator.x)/2.0f, float(neighborIterator.y)/2.0f, 1.0);
-
-      pixelDisplay->setPixel(neighborIterator, color);
-
-      std::cout << "("<<neighborIterator.x<<", "<<neighborIterator.y<<"): " << thisMass << std::endl;
-    }
-  }*/
-
   gridModel->injectSpecies(glm::ivec2(50,80), 1, 60.0f);
 }
 
